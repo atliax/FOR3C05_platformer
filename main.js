cd23c0c276469340be55d6d3a0f2b4f3a3b8aa6e
CapsuleerCoder <oli0202@gmail.com>
2024-03-20 23:48:52 +0000

    git í vesen

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 canvas = document.getElementById('mainCanvas');
 context = canvas.getContext('2d');
 
-
 //key virkni sem ég fann á netinu
 const KEY_LEFT = 37;
 const KEY_RIGHT = 39;
@@ -11,9 +10,6 @@ const KEY_SPACE = 32;
 
 document.addEventListener("keydown",keydown);
 document.addEventListener("keyup",keyup);
-
-const backgroundImage = new Image()
-backgroundImage.src = "bar.jpg" // bara placeholder mögulega.
 
 //key virkni sem ég fann á netinu
 function keydown(event)
@@ -35,92 +31,180 @@ function keyup(event)
     }
 }
 
-const keys = []
-
-const enemies = []
-
-const enemy_bullet = [];
-const hero_bullet = [];
-
-const hero =  
-{
-    x: canvas.width/2.3,
-    height: 100,
-    y: canvas.height - 100, 
-    width: 80,
-    speed: 5,
-    velocityX: 0,
-    velocityY: 0,
-    jumping: false,
-    shootDelay: 600,
-    lastshotTime: 0,
-
-    draw()
-    {
-        context.fillStyle = "blue";
-        context.fillRect(hero.x, hero.y, hero.width, hero.height);
-        hero.velocityX *= 0.9;
-        hero.velocityY += 0.5;
-        hero.x += hero.velocityX;
-        hero.y += hero.velocityY;
-        if (hero.x >= canvas.width - hero.width)
-        {
-            hero.x =  0
-        }   else if (hero.x <= 0) 
-        {
-            hero.x = canvas.width-hero.width
-        }
-    
-        if (hero.y >= canvas.height - hero.height)
-        {
-            hero.y = canvas.height - hero.height
-            hero.jumping = false
-        }
-    },
-    shoot(){
-    const currentTime = new Date().getTime();
-    if (currentTime - hero.lastshotTime > hero.shootDelay)
-
-    {
-        const bullet_speed = -5 ;
-        const bullet = new Bullet(hero.x+(hero.width*0.5), hero.y+hero.height, bullet_speed);
-        hero_bullet.push(bullet);
-        hero.lastshotTime  =currentTime
-    }
-    }
-}
-
 //key virkni sem ég fann á netinu
 function handleKeys()
 {
     if(keys[KEY_LEFT] == true)
     {
-        if (hero.velocityX > -hero.speed)
-        {
-            hero.velocityX--;
-        }
+        hero.move(-hero.speed);
     }
 
     if(keys[KEY_RIGHT] == true)
     {
-        if (hero.velocityX < hero.speed)
-        {
-            hero.velocityX++;
-        }
+        hero.move(hero.speed);
     }
 
     if(keys[KEY_UP] == true)
     {
-        if (!hero.jumping)
-        {
-            hero.velocityY = -hero.speed *2;
-            hero.jumping = true
-        }
+        hero.jump();
     }
 
     if(keys[KEY_SPACE] == true)
     {
-        hero.shoot()
+        hero.shoot();
+    }
+}
+
+class Hero  
+{
+    constructor()
+    {
+        this.width = 80;
+        this.height = 100;
+        this.x = canvas.width/2.3;
+        this.y = canvas.height - this.height;
+        this.speed = 5;
+        this.velocityX = 0;
+        this.velocityY = 0;
+        this.jumping = false;
+        this.shootDelay = 600;
+        this.lastshotTime = 0;
+    }
+
+    move(speedchange)
+    {
+        if(speedchange < 0)
+        {
+            if(this.velocityX > speedchange)
+            {
+                this.velocityX--;
+            }
+        }
+        else
+        {
+            if(this.velocityX < speedchange)
+            {
+                this.velocityX++;
+            }
+        }
+
+        this.velocityX *= 0.9;
+        this.velocityY += 0.5;
+        this.x += this.velocityX;
+        this.y += this.velocityY;
+
+        if (this.x >= canvas.width)
+        {
+            this.x = 0;
+        }
+        else if (this.x <= 0 - this.width) 
+        {
+            this.x = canvas.width - this.width;
+        }
+    
+        if (this.y >= canvas.height - this.height)
+        {
+            this.y = canvas.height - this.height;
+            this.jumping = false;
+        }
+    }
+
+    draw()
+    {
+        context.fillStyle = "blue";
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    shoot()
+    {
+        const currentTime = new Date().getTime();
+        if(currentTime - this.lastshotTime > this.shootDelay)
+        {
+            const bullet_speed = -5;
+            const bullet = new Bullet(this.x+(this.width*0.5), this.y+this.height, bullet_speed);
+            hero_bullet.push(bullet);
+            this.lastshotTime = currentTime;
+        }
+    }
+
+    jump()
+    {
+        if(!this.jumping)
+        {
+            this.velocityY = -this.speed * 2;
+            this.jumping = true;
+        }
+    }
+}
+
+class Bullet 
+{
+    constructor(x, y, speed)
+    {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.width = 10;
+        this.height = 10;
+    }
+
+    move()
+    {
+        this.y += this.speed;
+    }
+
+    draw()
+    {
+        context.fillStyle = "black";
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Goblin
+{
+    constructor()
+    {
+        this.x = Math.random()*15;
+        this.y = 60;
+        this.width = 200;
+        this.height = 35;
+        this.speed = 2;
+        this.lastshotTime = 0;
+        this.shootDelay = 1000;
+    }
+
+    move()
+    {
+        this.x += this.speed;
+
+        if (this.x+this.width >= canvas.width)
+        {
+            this.speed *= -1;
+        }
+
+        if (this.x+this.width <= this.width)
+        {
+            this.speed *= -1;
+        }
+    }
+
+    draw()
+    {
+        context.fillStyle = "green";
+        context.fillRect(this.x, this.y, this.width, this.height, this.speed);
+    }
+
+    shoot()
+    {
+        const currentTime = new Date().getTime();
+
+        if (currentTime - this.lastshotTime > this.shootDelay)
+        {
+            const bullet_speed = 5;
+            const bullet = new Bullet(this.x, this.y+this.height, bullet_speed);
+            enemy_bullet.push(bullet);
+            this.lastshotTime = currentTime;
+        }
     }
 }
 
@@ -140,9 +224,11 @@ function check_collision(bullet, object)
         bulletleft < objectright &&
         bulletbot > objecttop && 
         bullettop < objectbot)
-        {return true}
+    {
+        return true;
+    }
 
-    return false
+    return false;
 }
 
 function collision_consequence()
@@ -158,124 +244,176 @@ function collision_consequence()
             }
         }
     }
+
     for (let i= 0; i < enemy_bullet.length; i++)
     {
         if (check_collision(enemy_bullet[i], hero))
         {
             enemy_bullet.splice(i, 1);
-            console.log("hero dead")
+            console.log("hero dead");
         }  
     }
 }
 
-
-class Bullet 
+function enemies_move()
 {
-    constructor(x, y, speed)
+    if(enemies.length > 0)
     {
-        this.x = x;
-        this.y = y;
-        this.speed = speed;
-        this.width = 10
-        this.height = 10
-    }
-
-    draw()
-    {
-        context.fillStyle = "black";
-        this.y += this.speed;
-        
-        context.fillRect(this.x, this.y, this.width, this.height)
-
-        
+        for (let i = 0; i < enemies.length; i++)
+        {
+            enemies[i].move();
+        }
     }
 }
-class Goblin
-{
-    constructor()
-    {
-        this.x = Math.random()*15
-        this.y = 60
-        this.width = 200
-        this.height = 35
-        this.speed = 2
-        this.lastshotTime = 0
-        this.shootDelay = 1000
-    }
-    draw()
-    {
-        context.fillStyle = "green"
-        this.x += this.speed
-        if (this.x+this.width >= canvas.width)
-        {
-            this.speed *= -1
-        }
-        if (this.x+this.width <= this.width)
-        {
-            this.speed *= -1
-        }
-        context.fillRect(this.x, this.y, this.width, this.height, this.speed)
-    }
-    shoot()
-    {
-        const currentTime = new Date().getTime();
-        if (currentTime - this.lastshotTime > this.shootDelay)
-        {
-            const bullet_speed = 5 ;
-            const bullet = new Bullet(this.x, this.y+this.height, bullet_speed);
-            enemy_bullet.push(bullet);
-            this.lastshotTime  =currentTime
-        }
-    }
 
-}
-
-function draw_enemies()
+function enemies_draw()
 {
-    for (let i = 0; i < enemies.length; i++)
+    if(enemies.length > 0)
     {
-        enemies[i].draw()
-        enemies[i].shoot()
+        for (let i = 0; i < enemies.length; i++)
+        {
+            enemies[i].draw();
+        }
     }
 }
 
 function enemies_shoot()
 {
-    for (let i = 0; i < enemies.length; i++)
+    if(enemies.length > 0)
     {
-        for (let i = 0; i < enemy_bullet.length; i++) {
-            enemy_bullet[i].draw();
-        }  
-        for (let i = 0; i < hero_bullet.length; i++) {
-            hero_bullet[i].draw();
-        }        
+        for (let i = 0; i < enemies.length; i++)
+        {
+            enemies[i].shoot();
+        }
+    }
+}
 
+function bullets_move()
+{
+    if(enemy_bullet.length > 0)
+    {
+        for (let i = 0; i < enemy_bullet.length; i++)
+        {
+            enemy_bullet[i].move();
+        }
+    }
+
+    if(hero_bullet.length > 0)
+    {
+        for (let i = 0; i < hero_bullet.length; i++)
+        {
+            hero_bullet[i].move();
+        }
+    }
+}
+
+function bullets_draw()
+{
+    if(enemy_bullet.length > 0)
+    {
+        for (let i = 0; i < enemy_bullet.length; i++)
+        {
+            enemy_bullet[i].draw();
+        }
+    }
+
+    if(hero_bullet.length > 0)
+    {
+        for (let i = 0; i < hero_bullet.length; i++)
+        {
+            hero_bullet[i].draw();
+        }
+    }
+}
+
+const levelWidth = 20;
+const levelHeight = 17;
+const levelScale = 64;
+
+let levelData = [
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+];
+
+function draw_background()
+{
+    context.clearRect(0,0, canvas.width, canvas.height);
+
+    for(let y = 0; y < levelHeight; y++)
+    {
+        for(let x = 0; x < levelWidth; x++)
+        {
+            let index = (y * levelWidth) + x;
+
+            if(levelData[index] == 1)
+            {
+                context.fillStyle = "black";
+            }
+            else if(levelData[index] == 2)
+            {
+                context.fillStyle = "green";
+            }
+            else
+            {
+                context.fillStyle = "white";
+            }
+            context.fillRect(x*levelScale,y*levelScale,levelScale,levelScale);
+        }
     }
 }
 
 function draw_game()
 {
-   
-    context.clearRect(0,0, canvas.width, canvas.height);
-    //context.fillStyle = "white";
-    context.drawImage(backgroundImage, 0,0, canvas.width, canvas.height );
-    handleKeys()
-    hero.draw()
-    draw_enemies()
-    enemies_shoot()
-    collision_consequence()
+    draw_background();
 
+    hero.draw();
+    enemies_draw();
 
-
-
-    requestAnimationFrame(draw_game);
-    
+    bullets_draw();
 }
 
+function main_loop()
+{
+    handleKeys();
+
+    hero.move();
+    enemies_move();
+    bullets_move();
+
+    enemies_shoot();
+
+    collision_consequence();
+
+    draw_game();
+
+    requestAnimationFrame(main_loop);    
+}
+
+const keys = [];
+
+const enemies = [];
+
+const enemy_bullet = [];
+const hero_bullet = [];
+
+const hero = new Hero();
+
 //bara til að testa
-enemies.push(new Goblin())
+enemies.push(new Goblin());
 
-requestAnimationFrame(draw_game);
-
-
-
+requestAnimationFrame(main_loop);
